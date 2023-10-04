eacce8ebaeb2cca4869626937889a3a0412b0512
Xnotx731 <dubeynikhil791@gmail.com>
2023-10-04 15:35:09 +0530

    Fetch api and news api intergration with previous and next button

--- src/components/News.js
+++ src/components/News.js
@@ -2,41 +2,55 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    article=[
-        {
-            "source": {
-                "id": "bbc-sport",
-                "name": "BBC Sport"
-            },
-            "author": null,
-            "title": "Beaumont & Brook named PCA players of the year",
-            "description": "England batters Tammy Beaumont and Harry Brook are named the Professional Cricketers' Association women's and men's players of the year.",
-            "url": "http://www.bbc.co.uk/sport/cricket/66987161",
-            "urlToImage": "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/972D/production/_131310783_bbc-sport-index-imagery-2-split-images-gradient-dc0ca7c2-ffb0-445b-9720-17ed38ffab11.png",
-            "publishedAt": "2023-10-02T22:22:19.1851076Z",
-            "content": "England batters Tammy Beaumont and Harry Brook have been named the Professional Cricketers' Association women's and men's players of the year. \r\nIn the Ashes Test against Australia in June, opener Be… [+2559 chars]"
-        }
-    ]
+    article=[]
 
     constructor(){
         super();
         console.log("Welcome to universe");
          this.state={
          article:this.article,
-        loading: false}
+        loading: false,
+       page:1}
+        
 
     }
-  render() {
+    async componentDidMount(){
+      let url="https://newsapi.org/v2/top-headlines?country=us&apiKey=5db84a1ff88445f6a9b4ee1852daf246&page=1";
+      let data=await fetch(url);
+      let parsedData=await data.json();
+      this.setState({article:parsedData.articles,totalResults:parsedData.totalResults})
+  
+    }
+    handleprev=async()=>{
+   
+      let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=5db84a1ff88445f6a9b4ee1852daf246&page=${this.state.page-1}`;
+      let data=await fetch(url);
+      let parsedData=await data.json();
+      this.setState({article:parsedData.articles,
+          page:this.state.page-1})
+    }
+    handlenext=async()=>{
+
+      let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=5db84a1ff88445f6a9b4ee1852daf246&page=${this.state.page+1}`;
+      let data=await fetch(url);
+      let parsedData=await data.json();
+      this.setState({article:parsedData.articles,
+      page:this.state.page+1})
+    }
+    render() {
     return (
       <div className='container my-3' >
         <h2>NewsMonkey-Top Headlines</h2>
         <div className="row">
         {this.state.article.map((element)=>{
-           { console.log(element)}
-        <div className="col-md-4" key={element.url}>
-        <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+        return <div className="col-md-4" key={element.url}>
+        <NewsItem title={element.title} description={element.description} imageUrl={!element.urlToImage?"https://www.reuters.com/resizer/cxpRVXQCZVKvUa1zFhvsYw_eAOU=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/2BF2FX67VVPCZA42YGNBLZBN7U.jpg":element.urlToImage} newsUrl={element.url} />
         </div>
         })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-primary " onClick={this.handleprev} >&larr; Previous</button>
+        <button disabled={(Math.ceil(this.state.totalResults/12)<=(this.state.page+1))} type="button" className="btn btn-primary"  onClick={this.handlenext}>Next &rarr;</button>
         </div>
         
       </div>
